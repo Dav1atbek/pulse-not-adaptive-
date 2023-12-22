30b33c063ad4455ac57a9546d369613a11ed8c7c
Davlatbek <davlkatbekabdulhaev@gmail.com>
2023-12-22 12:41:46 +0600

    fix: buy  button ,add modal

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+// let formBtns = document.querySelectorAll(".button-buy");
+// let form = document.querySelector(".pop-up");
+// let formActive = document.querySelector(".pop-up_active");
+// let formClose = document.querySelectorAll(".close");
+
+// function popupOn() {
+//     form.classList.add("pop-up_active");
+
+// }
+
+// function popupOff() {
+//     form.classList.remove("pop-up_active");
+
+
+// }
+
+// formBtns.forEach((formBtn) => { 
+//     formBtn.addEventListener("click", function () {
+//         popupOn();
+//     });
+// });
+
+// // Добавление обработчика событий для каждой кнопки закрытия
+// formClose.forEach((formClose) => {
+//     formClose.addEventListener("click", function () {
+//         popupOff();
+//     });
+// });
--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,78 @@
-$('.catalog-heart__slick').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  speed:1000,
-  autoplay: true,
-  autoplaySpeed: 2500,
+$(document).ready(() => {
+  $('.catalog-heart__slick').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed:1000,
+    autoplay: true,
+    autoplaySpeed: 2500,
+  });
   
+  $('ul.catalog-tabs').on('click', 'li:not(.catalog-tab_active)', function () {
+      $(this)
+          .addClass('catalog-tab_active').siblings().removeClass('catalog-tab_active')
+          .closest('div.container').find('div.catalog-content').removeClass('catalog-content_active').eq($(this).index()).addClass('catalog-content_active');
+  });
+  
+  $('.catalog-item__link').each(function (i) {
+    $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+  })
+  
+  $('.catalog-item__back').each(function (i) {
+    $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+  })
+
+
+  // modal
+  
+  $('[data-modal=consultation]').on('click', function(){
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
+
+
+  $('.button_buy').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__name').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  })1
+  $('.button_buy').on('click', function() {
+    $('.button_buy').fadeIn('slow');
+  });
+  $('.button_buy').on('click', function() {
+    $('.order').fadeOut('slow');
+  });
+//   $('[data-modal=consultation]').on('click', function () {
+//     $('.overlay, #consultation').fadeIn('slow');
+// });
+// $('.modal__close').on('click', function () {
+//     $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+// });
+
+// $('.button_buy').each(function (i) {
+//     $(this).on('click', function () {
+//         $('#order .modal__descr').text($('.catalog-item__name').eq(i).text());
+//         $('.overlay, #order').fadeIn('slow');
+//     });
+// });
+
+  // $('.button_submit').on('click', function(){
+  //   $('.modal_mini').fadeIn('slow');
+  // });
+  // $('.button_submit').on('click', function(){
+  //   $('.modal_mini').fadeOut('slow');
+  // });
 });
+
+
